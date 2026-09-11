5c9f9c0bd537eac4074813f89e82bb0534038e6c
Rafael da Silva Sousa <141595662+rs-rafaelsousa@users.noreply.github.com>
2026-09-11 14:28:25 -0300

    Add files via upload

---
+++ js/louvores.js
@@ -0,0 +1,27 @@
+const bancoDeMusicas = {
+    "aquilo-que-fui": {
+        titulo: "Aquilo que fui não sou mais",
+        autor: "Let. J.J.M / Mús. I.A.M. • 2018",
+        tomOriginal: "Em",
+        tomDesc: "Mim",
+        conteudo: [
+            { acordes: ["Em", "C"], letra: "Aquilo que fui não sou mais," },
+            { acordes: ["B7"], letra: "Nem sou tudo o que devo ser;" },
+            { acordes: ["Am"], letra: "Já sou pela graça o que sou" },
+            { acordes: ["C"], letra: "E ao vê-lo estou certo que vou" },
+            { acordes: ["B7", "Em", "E"], letra: "Na glória de Cristo viver." },
+            { tipo: "titulo", texto: "Coro" },
+            { acordes: ["Am", "D", "G", "C", "B7"], letra: "Marana - ta, Maranata," }
+        ]
+    },
+    "castelo-forte": {
+        titulo: "Castelo Forte",
+        autor: "Martinho Lutero • 1529",
+        tomOriginal: "C",
+        tomDesc: "Dó",
+        conteudo: [
+            { acordes: ["C", "G"], letra: "Castelo forte é o nosso Deus," },
+            { acordes: ["Am", "F"], letra: "Defesa e espada nossa;" }
+        ]
+    }
+};
